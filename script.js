e66dbe4326197828bcc10e18a345eeb417b38d4c
yoginiatkari <yoginiatkari9@gmail.com>
2022-01-27 01:24:16 +0530

    git push

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const api_url = "https://myflaskyogini2.herokuapp.com/"
-function loadData(rec = []) {
+
+function loadData(records = []) {
         var table_data = "";
-        for(let i=0; i<rec.length; i++) {
+        for(let i=0; i<records.length; i++) {
                 table_data += `<tr>`;
 
-                table_data += `<td>${rec[i][0]}</td>`;
-                table_data += `<td>${rec[i][1]}</td>`;
-                table_data += `<td>${rec[i][2]}</td>`;
+                table_data += `<td>${records[i][0]}</td>`;
+                table_data += `<td>${records[i][1]}</td>`;
+                table_data += `<td>${records[i][2]}</td>`;
 
-                table_data += `<td>${rec[i][3]}</td>`;
+                table_data += `<td>${records[i][3]}</td>`;
+                table_data += `<td>`;
+                table_data += `<a href="edit.html?id=${records[i][0]}"><button class="btn btn-primary">Edit</button></a>`;
+                table_data += '&nbsp;&nbsp;';
+                table_data += `<button class="btn btn-danger" onclick=deleteData('${records[i][0]}')>Delete</button>`;
+                table_data += `</td>`;
                 table_data += `</tr>`;
         }
         console.log(table_data);
@@ -16,7 +22,7 @@ function loadData(rec = []) {
 }
 
 function getData() {
-        //alert("Hello World");
+        alert("Hello World");
         fetch(api_url)
         .then((response) => response.json())
         .then((data) => {
@@ -25,4 +31,5 @@ function getData() {
                 loadData(data);
         });
 }
+
 
